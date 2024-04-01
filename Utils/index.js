d6df1dd2cf3b0def21549c7738ad946030fc5fe3
Moyan Agrawal <115362065+MoyanAgrawal@users.noreply.github.com>
2024-04-01 19:48:21 +0530

    Update index.js

--- Utils/index.js
+++ Utils/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
  const dbConnection = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/Task-Manager");
+    await mongoose.connect("mongodb+srv://Moyan_Agrawal:Moy%40n144@cluster0.8dwfmja.mongodb.net/Task-Manager");
 
     console.log("DB connection established");
   } catch (error) {
